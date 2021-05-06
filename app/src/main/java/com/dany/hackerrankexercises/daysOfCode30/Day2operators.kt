8292c03f40d9a5f1718cf76849fb6ff53bd5d601
danyramirezg <danyramirezg@gmail.com>
2021-05-05 21:55:37 -0500

    Day 2 challenge operators

---
+++ app/src/main/java/com/dany/hackerrankexercises/daysOfCode30/Day2operators.kt
@@ -0,0 +1,82 @@
+package com.dany.hackerrankexercises.daysOfCode30
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+import kotlin.math.round
+
+/*
+Objective
+In this challenge, you will work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+
+Example
+
+
+
+A tip of 15% * 100 = 15, and the taxes are 8% * 100 = 8. Print the value  and return from the function.
+
+Function Description
+Complete the solve function in the editor below.
+
+solve has the following parameters:
+
+int meal_cost: the cost of food before tip and tax
+int tip_percent: the tip percentage
+int tax_percent: the tax percentage
+Returns The function returns nothing. Print the calculated value, rounded to the nearest integer.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result.
+
+Input Format
+
+There are  lines of numeric input:
+The first line has a double,  (the cost of the meal before tax and tip).
+The second line has an integer,  (the percentage of  being added as tip).
+The third line has an integer,  (the percentage of  being added as tax).
+ */
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+fun solve(meal_cost: Double, tip_percent: Int, tax_percent: Int): Unit {
+
+    val tip = (meal_cost * tip_percent) / 100
+    val tax = (meal_cost * tax_percent) / 100
+    val result = round(meal_cost + tip + tax)
+    println(result.toInt())
+
+}
+
+fun main(args: Array<String>) {
+    val meal_cost = readLine()!!.trim().toDouble()
+
+    val tip_percent = readLine()!!.trim().toInt()
+
+    val tax_percent = readLine()!!.trim().toInt()
+
+    solve(meal_cost, tip_percent, tax_percent)
+}
