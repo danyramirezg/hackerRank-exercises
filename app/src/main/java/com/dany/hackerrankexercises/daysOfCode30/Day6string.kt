4f44a00eab73f65f39507b7a5d8d5a7e36b30031
danyramirezg <danyramirezg@gmail.com>
2021-05-09 18:35:11 -0500

    Challenge day 6 -Strings

---
+++ app/src/main/java/com/dany/hackerrankexercises/daysOfCode30/Day6string.kt
@@ -0,0 +1,27 @@
+package com.dany.hackerrankexercises.daysOfCode30
+
+/*
+    Given a string, S, of length N that is indexed from 0 to N -1, print its even-indexed and
+    odd-indexed characters as 2 space-separated strings on a single line
+
+    Note: 0 is considered to be an even index.
+
+    Example:
+
+    s = abdecf
+
+    Output:
+    abc def
+
+    Input Format
+
+    The first line contains an integer, T (the number of test cases).
+    Each line i of the  subsequent lines contain a string, S.
+ */
+
+
+
+
+
+
+
