27c2e12c55de6d0ae4b8a0ffcc34a627097b4356
danyramirezg <danyramirezg@gmail.com>
2021-05-14 10:56:02 -0500

    Challenge day 9 - Recursion

--- app/src/main/java/com/dany/hackerrankexercises/daysOfCode30/Day9recursion.kt
+++ app/src/main/java/com/dany/hackerrankexercises/daysOfCode30/Day9recursion.kt
@@ -12,13 +12,14 @@ fun summation(n: Int): Int {
   else {
 
     // 3 + summation (3-1)
-    // 2 + summation (2-1)
-    // 1 + summation (1-1)
-    // 3 + 2 + 1 = 6
+    // 3 + 2 + summation (2-1)
+    // 3 + 2 + 1 + summation (1-1)
+    // 3 + 2 + 1 + 0 = 6
     return n + summation(n - 1)
   }
 }
 
+// 4! = 4 * 3 * 2 * 1
 fun factorial(n: Int): Int {
 
   // Base case:
@@ -27,18 +28,41 @@ fun factorial(n: Int): Int {
   }
   // Recursive case
   else {
-    return (n * factorial( n - 1))
+    // 4 * factorial(4 -1)
+    // 4 * 3 * factorial(3 - 1)
+    // 4 * 3 * 2 * factorial(2 -1)
+    // 4 * 3 * 2 * 1
+    return (n * factorial(n - 1))
+  }
+}
+
+// 2^3 = 2 * 2 * 2
+fun exponentiation(n: Int, p: Int): Int {
+
+  if (p <= 0) {
+    return 1
+  } else {
+    return n * exponentiation(n, p - 1)
+    // 2 * exponentiation(2, 2)
+    // 2 * 2 * exponentiation(2, 1)
+    // 2 * 2 * 2 * exponentiation(2, 0)
+    // 2 * 2 * 2 * 1
   }
 
 }
 
 fun main(args: Array<String>) {
   val n = readLine()!!.trim().toInt()
+  val p = readLine()!!.trim().toInt()
+
 
   val result = factorial(n)
 
   val sum = summation(n)
 
+  val expon = exponentiation(n, p)
+
   println("The factorial of $n is: $result")
   println("The summation of the $n is $sum")
+  println("$n elevado al $p es $expon")
 }
