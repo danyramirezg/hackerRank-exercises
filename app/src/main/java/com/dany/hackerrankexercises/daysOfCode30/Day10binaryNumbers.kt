f4ed2f0e5dd6be7fa1a7611fd239df5c9e3a4f01
danyramirezg <danyramirezg@gmail.com>
2021-05-16 14:48:14 -0500

    Challenge day 10 binary numbers

--- app/src/main/java/com/dany/hackerrankexercises/daysOfCode30/Day10binaryNumbers.kt
+++ app/src/main/java/com/dany/hackerrankexercises/daysOfCode30/Day10binaryNumbers.kt
@@ -1,5 +1,33 @@
 package com.dany.hackerrankexercises.daysOfCode30
 
+/*
+  Task
+  Given a base-10 integer, n, convert it to binary (base-2). Then find and print the base-10 integer
+  denoting the maximum number of consecutive 1's in n's binary representation. When working with
+  different bases, it is common to show the base as a subscript.
+
+  Example:
+  n = 125
+
+  The binary representation of 125 base 10 is 1111101. In base 10, there are 5 and 1 consecutive
+  ones in two groups. Print the maximum, 5.
+
+  Input: 5
+  Output: 1
+
+  Input: 13
+  Output: 2
+
+  Explanation:
+
+  Sample Case 1:
+  The binary representation of 5 is 101, so the maximum number of consecutive 1's is .
+
+  Sample Case 2:
+  The binary representation of 13 is 1101, so the maximum number of consecutive 1's is 2.
+
+ */
+
 import kotlin.math.max
 
 fun main(args: Array<String>) {
